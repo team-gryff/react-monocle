6e4be44fb7ff3c771bf524f522fd12f8eb2ec827
Michael-Bryant Choa <mbchoa@gmail.com>
2016-06-28 21:28:52 -0700

    refactor es5 react component parser to handle parsing of nested children by 1 level

--- src/reactParser.js
+++ src/reactParser.js
@@ -3,10 +3,20 @@
 const acorn = require('acorn-jsx');  
 const esrecurse = require('esrecurse');  
 
+
+const htmlElements = ['a', 'article', 'audio', 'b', 'body', 'br', 'button', 'canvas', 'caption',
+                      'code', 'dialog', 'div', 'dl', 'dt', 'em', 'embed', 'font', 'footer', 'form',
+                      'frame', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'html', 'i', 'iframe', 'img',
+                      'input', 'kbd', 'label', 'legend', 'li', 'link', 'main', 'map', 'noscript',
+                      'object', 'ol', 'option', 'p', 'param', 'pre', 'progress', 'q', 'rb', 'rt',
+                      'ruby', 's', 'samp', 'script', 'section', 'select', 'small', 'source', 'span',
+                      'strong', 'style', 'sub', 'summary', 'table', 'tbody', 'td', 'thead', 'title',
+                      'tr', 'track', 'u', 'ul', 'var', 'video', 'wbr'];
+
 let jsAst;
 
 function getES5ReactComponents() {
-  let output = [];
+  let output = {};
   let currVariableName;
   esrecurse.visit(jsAst, {
     VariableDeclarator: function (node) {
@@ -16,13 +26,35 @@ function getES5ReactComponents() {
     MemberExpression: function (node) {
       if (currVariableName) {
         if (node.property && node.property.name === "createClass") {
-          output.push({
-            name: currVariableName
-          });
-          currVariableName = undefined;
+          output.name = currVariableName
         }
       }
-    }
+    },
+    Property: function (node) {
+      if (node.key.name === "render") {
+        this.visitChildren(node);
+      }
+    },
+    ReturnStatement: function (node) {
+      if (node.argument.type === 'JSXElement') {
+        this.visitChildren(node);
+      }
+    },
+    JSXElement: function (node) {
+      var jsxComponentsArr = node
+        .children
+        .filter(jsx => {
+          return jsx.type === "JSXElement" &&
+            htmlElements.indexOf(jsx.openingElement.name.name) < 0
+        });
+      for (let prop in output) {
+        if (output[prop] === currVariableName) {
+          output.children = jsxComponentsArr.map(jsx => {
+            return { name: jsx.openingElement.name.name };
+          });
+        }
+      }
+    },
   });
   return output;
 }
@@ -33,13 +65,11 @@ function isReactComponent (node) {
 }
 
 function getES6ReactComponents() {
-  let output = [];
+  let output = {};
   esrecurse.visit(jsAst, {
     ClassDeclaration: function (node) {
       if (isReactComponent(node)) {
-        output.push({
-          name: node.id.name,
-        });
+        output.name = node.id.name
       }
     }
   });
@@ -47,7 +77,9 @@ function getES6ReactComponents() {
 }
 
 function jsToAst(js) {
-  jsAst = acorn.parse(js);
+  jsAst = acorn.parse(js, { 
+    plugins: { jsx: true }
+  });
   if (jsAst.body.length === 0) throw new Error('Empty AST input');
 }
 
