e70c368e349e5867131557f542ba02ac036efcc4
jerrymao15 <jerryy.mao@gmail.com>
2016-06-29 13:56:50 -0700

    refactored astGenerator to determine entry point. refactored cmd.js to do the same and to accept optional parameters

--- src/test/astGeneratorTest.js
+++ src/test/astGeneratorTest.js
@@ -1,7 +1,8 @@
 'use strict';
 const expect = require('chai').expect;
-const es5Component = __dirname + '/fixtures/test_components/Foo_es5.jsx'
+const es5Component = __dirname + '/fixtures/test_components/Foo_es5.jsx';
 const es6Component = __dirname + '/fixtures/test_components/Foo_es6.jsx';
+const ReactDOMRender = __dirname + '/fixtures/test_components/ReactDOMRender.jsx';
 const astGenerator = require('../astGenerator');
 
 describe('astGenerator Tests for ES5 Components', function() {
@@ -42,4 +43,18 @@ describe('astGenerator Tests for ES6 Components', function() {
     expect(astGenerator(es6Component).hasOwnProperty('Foo')).to.be.true;
   })
 
+})
+
+describe('astGenerator Tests to find entry', function() {
+  it('should return an object', function() {
+    expect(astGenerator(ReactDOMRender)).to.be.an('object');
+  })
+
+  it('should return an object with one key', function() {
+    expect(Object.keys(astGenerator(ReactDOMRender)).length).to.equal(1);
+  })
+
+  it('should have key called "ENTRY', function() {
+    expect(astGenerator(ReactDOMRender).hasOwnProperty('ENTRY')).to.be.true;
+  })
 })
