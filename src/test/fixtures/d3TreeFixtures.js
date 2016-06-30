b00031156c7c9ed8cd6b401f8bddfd295f4eb752
Michael-Bryant Choa <mbchoa@gmail.com>
2016-06-29 20:52:32 -0700

    add npm script to bundle d3tree requires, clean up d3Tree script

---
+++ src/test/fixtures/d3TreeFixtures.js
@@ -0,0 +1,26 @@
+module.exports = {
+  testData1: [{
+    "name": "Top Level",
+    "parent": "null",
+    "children": [
+      {
+        "name": "Level 2: A",
+        "parent": "Top Level",
+        "children": [
+          {
+            "name": "Son of A",
+            "parent": "Level 2: A"
+          },
+          {
+            "name": "Daughter of A",
+            "parent": "Level 2: A"
+          }
+        ]
+      },
+      {
+        "name": "Level 2: B",
+        "parent": "Top Level"
+      }
+    ]
+  }],
+};
