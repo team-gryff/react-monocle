a892f861cf0cb3f6d1cff36f5b2fe8efbd7977de
jerrymao15 <jerryy.mao@gmail.com>
2016-06-29 17:31:27 -0700

    fixed cmd.js so it properly works with no params. changed test files to account for changes i made.

--- bin/cmd.js
+++ bin/cmd.js
@@ -2,25 +2,28 @@
 'use strict'
 const program = require('commander');
 const glob = require('glob');
-const astGenerator = require('../src/astGenerator.js');
+const astGenerator = require('../src/astGenerator');
 const assign = require('lodash.assign');
+const d3DataBuilder = require('../src/d3DataBuilder')
 
 // specifying one required parameter
 program
   .option('-e, --entry <entry>', 'App entry point. Defaults to JSX file where ReactDOM.render is found.')
   .option('-d, --directory <directory>', 'directory of React files. Defaults to where Monocle was called.')
-  .option('-f, --extension <extension>', 'extension of React files. Defaults to .jsx (only use when specifying/in directory which has your React files!)')
-  .action(function(options){
-    let entry = options.entry || null;
-    let directory = options.directory || process.cwd();
-    let ext = options.extension || 'jsx';
-    // globs to match any jsx in directory called
-    glob(`**/*.${ext}`, {cwd: directory, nosort:true, ignore: 'node_modules/**'}, (err, files) => {
-      const astz = files.map(ele => {return astGenerator(ele)})
-      const componentObject = assign.apply(null, astz); // combining into one file
-      if (entry) componentObject.ENTRY = astGenerator(entry);
-    })
-  })
+  .option('-j, --extension <extension>', 'extension of React files (jsx or js). Defaults to .jsx (only use when specifying/in directory which has your React files!)')
   .parse(process.argv);
 
 
+(function() {
+  const entry = program.entry || null;
+  const directory = program.directory || process.cwd();
+  const ext = program.extension || 'jsx';
+  // globs to match any jsx in directory called
+  glob(`**/*.${ext}`, {cwd: directory, nosort:true, ignore: 'node_modules/**'}, (err, files) => {
+    const astz = files.map(ele => {return astGenerator(ele)})
+    const componentObject = assign.apply(null, astz); // combining into one file
+    if (entry) componentObject.ENTRY = astGenerator(entry);
+    const formatedD3Object = d3DataBuilder(componentObject);
+    console.log(formatedD3Object);
+  })
+})()
