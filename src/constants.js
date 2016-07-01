f55b1d44c9039232372aa81c6c3f1f6131e3ca03
Michael-Bryant Choa <mbchoa@gmail.com>
2016-07-01 11:17:05 -0700

    add constants js file

---
+++ src/constants.js
@@ -0,0 +1,18 @@
+module.exports = {
+  htmlElements: [
+    'a', 'article', 'audio', 'b', 'body', 'br', 'button', 'canvas', 'caption',
+    'code', 'dialog', 'div', 'dl', 'dt', 'em', 'embed', 'font', 'footer', 'form',
+    'frame', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'html', 'i', 'iframe', 'img',
+    'input', 'kbd', 'label', 'legend', 'li', 'link', 'main', 'map', 'noscript',
+    'object', 'ol', 'option', 'p', 'param', 'pre', 'progress', 'q', 'rb', 'rt',
+    'ruby', 's', 'samp', 'script', 'section', 'select', 'small', 'source', 'span',
+    'strong', 'style', 'sub', 'summary', 'table', 'tbody', 'td', 'thead', 'title',
+    'tr', 'track', 'u', 'ul', 'var', 'video', 'wbr'
+  ],
+  reactMethods: [
+    'render', 'getInitialState', 'getDefaultProps', 'propTypes', 'mixins', 'statics',
+    'displayName', 'componentWillMount', 'componentDidMount', 'componentWillReceiveProps',
+    'shouldComponentUpdate', 'componentWillUpdate', 'componentDidUpdate',
+    'componentWillUnmount'
+  ]
+}
