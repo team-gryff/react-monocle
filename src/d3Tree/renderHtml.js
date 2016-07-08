3f3c6701afd03e917ac4911504f0d162425419a6
jerrymao15 <jerryy.mao@gmail.com>
2016-07-08 14:29:33 -0700

    imported updated react code to actual CLI

--- src/d3Tree/renderHtml.js
+++ src/d3Tree/renderHtml.js
@@ -5,14 +5,12 @@ const exec = require('child_process').exec;
 
 function renderHtml(d3Obj, start) {
   const treeHtmlPath = '/src/d3Tree/d3Tree.html',
-    d3libPath = '/node_modules/d3/d3.min.js',
-    treeLogicPath = '/src/d3Tree/d3Tree.js',
+    treeLogicPath = '/src/d3Tree/app.js',
     endHtmlPath = '/src/d3Tree/end.html',
     treeHtml = fs.readFileSync(appRoot + treeHtmlPath, { encoding: 'utf-8' }),
-    d3lib = fs.readFileSync(appRoot + d3libPath, { encoding: 'utf-8' }),
     treeLogic = fs.readFileSync(appRoot + treeLogicPath, { encoding: 'utf-8' }),
     endHtml = fs.readFileSync(appRoot + endHtmlPath, { encoding: 'utf-8' }),
-    insert = `${treeHtml} \n <script type="text/javascript"> \n ${d3lib} \n </script>
+    insert = `${treeHtml} \n </script>
         <script type="text/javascript"> \n var d3Obj = ${JSON.stringify(d3Obj, null, 2)} \n ${treeLogic} \n </script> \n ${endHtml}`;
     fs.writeFile(`${process.cwd()}/react-monocle.html`, insert, err => {
     if (err) throw new Error(err);
