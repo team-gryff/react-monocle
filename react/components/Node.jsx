54bfe9c15d7ec4d17bd1796357cecc44bffdc7a9
jerrymao15 <jerryy.mao@gmail.com>
2016-07-08 13:54:13 -0700

    refactored d3 tree with styling. added popups w/ table to nodes.

--- react/components/Node.jsx
+++ react/components/Node.jsx
@@ -1,44 +1,80 @@
 import React from 'react';
+import { Panel, Block, Heading, Text } from 'rebass';
+import Popover from 'react-popover';
+import NodeUp from './NodeUp.jsx';
 
 
-const Node = (props) => {
-  const style = {
-    transform: `translate(${props.xtranslate}px,${props.ytranslate}px)`,
-    width: props.width,
-    height: props.height,
-    cursor: 'pointer',
-    backgroundColor: '#FAFAFA',
-    borderRadius: '5px',
-    boxShadow: '0 0 1em #90A4AE',
-    fontSize: '14px',
-  };
-  const header = {
-    width: '100%',
-    height: Math.floor(props.height / 4),
-    backgroundColor: '#01579B',
-    color: 'white',
-    // fontSize: '20px',
-  };
-  let stateArr = [];
-  let propsArr = [];
-  let methodsArr = [];
-  if (props.state.length !== 0) stateArr = props.state.map(ele => { return ele.name; });
-  if (props.props.length !== 0) propsArr = props.props.map(ele => { return ele.name; });
-  if (props.methods.length !== 0) methodsArr = props.methods;
-  return (
-    <foreignObject onMouseEnter={props.highlight} onMouseLeave={props.lowlight}><div
-      className="node"
-      style={style}
-    >
-      <div style={header}>
-          <h4>{props.name.toUpperCase()}</h4>
-      </div>
-      State: {stateArr.join(', ')} <br />
-      Props: {propsArr.join(', ')} <br />
-      Methods: {methodsArr.join(', ')}
-    </div></foreignObject>
-  );
-};
+class Node extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { isOpen: false };
+    this.toggle = this.toggle.bind(this);
+  }
+
+  toggle() {
+    const bool = !this.state.isOpen;
+    return this.setState({ isOpen: bool });
+  }
+
+  render() {
+    const style = {
+      transform: `translate(${this.props.xtranslate}px,${this.props.ytranslate}px)`,
+      width: this.props.width,
+      height: this.props.height,
+      cursor: 'pointer',
+      backgroundColor: '#FAFAFA',
+      borderRadius: '5px',
+      boxShadow: '0 0 1em #90A4AE',
+      fontSize: '14px',
+      textDecoration: 'none',
+    };
+    // const header = {
+    //   width: '100%',
+    //   height: Math.floor(this.props.height / 4),
+    //   backgroundColor: '#01579B',
+    //   color: 'white',
+    //   // fontSize: '20px',
+    // };
+    let propsArr = [];
+    let methodsArr = [];
+    if (this.props.props.length !== 0) propsArr = this.props.props.map(ele => { return ele.name; });
+    if (this.props.methods.length !== 0) methodsArr = this.props.methods;
+
+    return (
+      <foreignObject onMouseEnter={this.props.highlight} onMouseLeave={this.props.lowlight} onClick={this.toggle}>
+      <Popover isOpen={this.state.isOpen} preferPlace="right" body={<NodeUp state={this.props.state} props={this.props.props} />}>
+        <Panel
+          className="node"
+          style={style}
+          theme="secondary"
+          p={0}
+          my={0}
+        >
+          <Block borderLeft
+            borderColor="#263238"
+            backgroundColor="#1976D2"
+            color="#FAFAFA"
+            px={2}
+            py={0}
+            my={0}
+          >
+            <Heading size={3}>
+            {this.props.name.toUpperCase()}
+            </Heading>
+          </Block>
+          <Text px={1}>
+            Props: <br />
+           {propsArr.join(' | ')} <br />
+          </Text>
+          <br />
+          <Text small px={1}>
+           Methods:
+           {methodsArr.join('| ')}
+          </Text>
+        </Panel></Popover></foreignObject>
+    );
+  }
+}
 
 Node.propTypes = {
   xtranslate: React.PropTypes.number,
--- webpack.config.js
+++ webpack.config.js
@@ -1,40 +1,46 @@
 const webpack = require('webpack');
 
 
-
 module.exports = {
   devtool: 'source-map',
-  entry:  [
-    "webpack-dev-server/client?http://localhost:9090",
-    "webpack/hot/only-dev-server",
-    "./react/index"
+  entry: [
+    'webpack-dev-server/client?http://localhost:9090',
+    'webpack/hot/only-dev-server',
+    './react/index',
   ],
 
   // This will not actually create a bundle.js file in ./client. It is used
   // by the dev server for dynamic hot loading.
   output: {
-    path: __dirname + "/client/",
-    filename: "app.js",
-    publicPath: "http://localhost:9090/client/"
+    path: __dirname + '/client/',
+    filename: 'app.js',
+    publicPath: 'http://localhost:9090/client/',
   },
-  // entry:'./src/main.jsx',
+  // entry: './react/index.jsx',
   // output: {
   //   path: './client',
   //   filename: 'bundle.js'
   // },
   resolve: {
-    extensions: ['', '.js', '.jsx']
+    extensions: ['', '.js', '.jsx'],
   },
   module: {
     loaders: [{
       test: /\.jsx?$/,
-      loaders: ['react-hot','babel-loader'],
-      exclude: /node_modules/
-    }]
+      loaders: ['react-hot', 'babel-loader'],
+      exclude: /node_modules/,
+    },
+    // {
+    //   loader: 'uglify',
+    // },
+    ],
   },
   plugins: [
     new webpack.HotModuleReplacementPlugin(),
     // new ExtractTextPlugin('style.css', { allChunks: true })
-    new webpack.NoErrorsPlugin()
+    new webpack.NoErrorsPlugin(),
+    // new webpack.optimize.UglifyJsPlugin({
+    //   mangle: false,
+    // }),
   ],
-}
+};
