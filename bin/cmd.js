d0fd1396c758917504e16181dc9a75c46f785c8e
Jenna <jenna@Jennas-MacBook-Air.local>
2016-07-09 10:59:14 -0700

    render user's app beside monocle tree

--- bin/cmd.js
+++ bin/cmd.js
@@ -6,10 +6,12 @@ const astGenerator = require('../src/astGenerator');
 const assign = require('lodash.assign');
 const d3DataBuilder = require('../src/d3DataBuilder');
 const renderHtml = require('../src/d3Tree/renderHtml');
+const previewParser = require('../src/previewParser')
 const start = Date.now();
 
 // specifying one required parameter
 program
+  .option('-b, --bundle <bundle>', '**Required** Path to react bundle file')
   .option('-e, --entry <entry>', 'App entry point. Defaults to JSX file where ReactDOM.render is found.')
   .option('-d, --directory <directory>', 'directory of React files. Defaults to where Monocle was called.')
   .option('-j, --extension <extension>', 'extension of React files (jsx or js). Defaults to .jsx (only use when specifying/in directory which has your React files!)')
@@ -18,11 +20,18 @@ program
 program.name = 'monocle';
 
 (function () {
+  let bundle = null;
   let entry = null;
   let directory = process.cwd();
+  if (!program.bundle) {
+    throw new Error('    --bundle | -b required    ');
+  } else {
+    bundle = `${process.cwd()}/${program.bundle}`;
+  }
   if (program.entry) entry = `${process.cwd()}/${program.entry}`;
   if (program.directory) directory = `${process.cwd()}/${program.directory}`;
   const ext = program.extension || 'jsx';
+  bundle = previewParser.modifyBundleFile(bundle);
   // globs to match any jsx in directory called
   // `**/*.js*` for both js and jsx
   glob(`**/*.${ext}`, { cwd: directory, nosort: true, ignore: ['node_modules/**', 'react/**'] }, (err, files) => { // TODO: CHANGE BACK
@@ -34,6 +43,6 @@ program.name = 'monocle';
     let componentObject = assign.apply(null, astz); // combining into one file
     if (entry) componentObject = assign(componentObject, astGenerator(entry, true));
     const formatedD3Object = d3DataBuilder(componentObject); // building the tree
-    renderHtml(formatedD3Object, start); // sending the completed tree to be built and rendered
+    renderHtml(formatedD3Object, bundle, start); // sending the completed tree to be built and rendered
   });
 })();
