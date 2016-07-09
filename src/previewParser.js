067ac08483f8bcfe82520ec536d67af2ee1ee823
Jenna <jenna@Jennas-MacBook-Air.local>
2016-07-08 19:46:52 -0700

    search user's bundle file for mount location and replace with uniform div id

--- src/previewParser.js
+++ src/previewParser.js
@@ -1,5 +1,10 @@
 'use strict';
 
+let bundled = `(0, _reactDom.render)(_react2.default.createElement(
+	  Frame,
+	  null,
+	  _react2.default.createElement(App, null)
+		), document.getElementById('app'));`
 
 function injectWrapper(bundlejs) {
   if (bundlejs.length === 0) throw new Error('Empty AST input');
@@ -8,7 +13,16 @@ function injectWrapper(bundlejs) {
   return bundlejs.replace(searchStr, wrappedFunc);
 }
 
+function updateMount(bundlejs) {
+  if (bundlejs.length === 0) throw new Error('Empty AST input');
+  const searchStr = /(getElementById\([\'\"])\w+[\'\"]/g;
+  const newMount = 'getElementById("preview"';
+  return bundlejs.replace(searchStr, newMount);
+}
+
+updateMount(bundled);
 
 module.exports = {
   injectWrapper,
+  updateMount,
 };
--- src/test/previewParserTest.js
+++ src/test/previewParserTest.js
@@ -4,18 +4,32 @@ const expect = require('chai').expect;
 
 describe('ReactApp AST Parser Tests', function() {
   const injectWrapper = require('../previewParser.js').injectWrapper;
+  const updateMount = require('../previewParser.js').updateMount;
   let previewParserFixtures = require('./fixtures/bundleFileFixture.js');
 
   it('injectWrapper should be a function', function() {
     expect(injectWrapper).to.be.a.function;
   });
 
-  it('should throw error when parser receives empty js code string', function() {
+  it('injectWrapper should throw error when parser receives empty js code string', function() {
     expect(injectWrapper.bind(injectWrapper,'')).to.throw(Error, /Empty AST input/); 
   });
 
-  it('should return string', function() {
+  it('injectWrapper should return a string', function() {
       expect(injectWrapper(previewParserFixtures.bundledSetState))
         .to.equal(previewParserFixtures.wrappedFunction);
   });
+
+  it('updateMount should be a function', function() {
+    expect(updateMount).to.be.a.function;
+  });
+
+  it('updateMount should throw error when parser receives empty js code string', function() {
+    expect(updateMount.bind(updateMount, '')).to.throw(Error, /Empty AST input/);
+  })
+
+  it('updateMount should return a string', function() {
+    expect(updateMount(previewParserFixtures.bundledGetElement)).to.equal(previewParserFixtures.newMount);
+  })
+
 })
