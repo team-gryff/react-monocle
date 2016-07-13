9f06403c35472baa2e0bc5e055be1105c3323bc6
Michael-Bryant Choa <mbchoa@gmail.com>
2016-07-13 10:23:14 -0700

    add getComponentName helper function

--- src/previewParser.js
+++ src/previewParser.js
@@ -1,5 +1,6 @@
 'use strict';
 const fs = require('fs');
+const regexLastIndexOf = require('./stringRegexHelper').regexLastIndexOf;
 
 function modifyBundleFile(bundlejs) {
   let bundle = fs.readFileSync(bundlejs, { encoding: 'utf-8' });
@@ -22,9 +23,40 @@ function modifyTestBundleFile(bundle) {
   return replacedState.replace(searchElem, newMount);
 }
 
+function getComponentName(bundle, startingIndex) {
+  let bundleSearchIndicesMap = {};
+  // get index of component declaration
+  bundleSearchIndicesMap[regexLastIndexOf(bundle, /var \w+ = \(\d+, _react.createClass\)/, startingIndex)] = 'GULP';
+  // let's try web pack...
+  bundleSearchIndicesMap[regexLastIndexOf(bundle, /var \w+ = function \(_React\$Component\)/, startingIndex)] = 'WEBPACK';
+  
+  const targetIndex = Object.keys(bundleSearchIndicesMap)
+  	.filter(index => index >= 0)
+  	.reduce((prev, curr) => {
+  	  return Math.abs(curr-startingIndex) < Math.abs(prev-startingIndex)
+  	  	? curr
+  	  	: prev;
+  	});
+ 
+  let componentMatch;
+  switch(bundleSearchIndicesMap[targetIndex]) {
+  	case 'GULP':
+  	  componentMatch = bundle.slice(targetIndex)
+  	  	.match(/var \w+ = \(\d+, _react.createClass\)/)
+  	  break;
+    case 'WEBPACK':
+   	  componentMatch = bundle.slice(targetIndex)
+  	    .match(/var \w+ = function \(_React\$Component\)/)
+   	  break;
+    default:
+    	break;
+  }
+  return componentMatch[0].split(' ')[1];
+}
 
 module.exports = {
   modifyBundleFile,
   modifyTestBundleFile,
+  modifySetStateStrings,
 };
 
