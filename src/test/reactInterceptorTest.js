05dd3a96059dda916ea85bd294e521aa74086b77
Jenna <jenna@Jennas-MacBook-Air.local>
2016-07-18 11:39:31 -0700

    send initial state to redux store

--- src/test/reactInterceptorTest.js
+++ src/test/reactInterceptorTest.js
@@ -3,13 +3,14 @@
 const expect = require('chai').expect;
 
 describe('React Interceptor Helper Tests', function() {
-  const monocleHook = require('../reactInterceptor.js');
+  const monocleHook = require('../reactInterceptor.js').reactInterceptor;
+  const initialStateWrapper = require('../reactInterceptor.js').grabInitialState;
 
-  it('should return a valid function', function() {
+  it('monocleHook should return a valid function', function() {
     expect(monocleHook).to.be.a('function');
   });
 
-  it('should execute callback provided when calling function closure', function() {
+  it('monocleHook should execute callback provided when calling function closure', function() {
     const callback = function() {
       this.counter++;
     };
@@ -19,4 +20,10 @@ describe('React Interceptor Helper Tests', function() {
     hookedCallback();
     expect(callback.counter).to.equal(1);
   });
+
+  it('initialStateWrapper should be a function', function() {
+    expect(initialStateWrapper).to.be.a('function');
+  });
+
+  
 });
