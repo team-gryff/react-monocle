3ef834f92e29f5d9144b7c13b917bb6159bcc1ac
Michael-Bryant Choa <mbchoa@gmail.com>
2016-07-29 16:18:34 -0700

    fix linter issues with reactInterceptor

--- src/reactInterceptor.js
+++ src/reactInterceptor.js
@@ -2,17 +2,17 @@ const monocleStore = require('../react/store/monocleStore');
 const updateState = require('../react/actions/index').updateState;
 const sendInitialState = require('../react/actions/index').sendInitialState;
 
-const reactInterceptor = function(name, component) { 
-  return function(state, callback) {
+export function reactInterceptor(name, component) {
+  return (state, callback) => {
     monocleStore.dispatch(updateState(name, state));
     return component.setState(state, callback);
-  }
+  };
 }
 
-const grabInitialState = function(name, obj) {
+export function grabInitialState(name, obj) {
   monocleStore.dispatch(sendInitialState(name, obj));
   return obj;
-};
+}
 
 module.exports = {
   reactInterceptor,
